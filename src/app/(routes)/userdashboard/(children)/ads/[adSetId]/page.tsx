a09175f11b6f8d90c9e87040573e3f357e8f1292
ronakpustack <ronak@pustack.com>
2024-02-20 15:12:21 +0530

    Minor key change

--- src/app/(routes)/userdashboard/(children)/ads/[adSetId]/page.tsx
+++ src/app/(routes)/userdashboard/(children)/ads/[adSetId]/page.tsx
@@ -13,6 +13,7 @@ import SwitchComponent from '@/app/components/helper/SwitchComponent';
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import metaMarketingApi from "../../../../../api/meta_marketing_api";
+import Link from "next/link";
 
 const Ads = () => {
     const params = useParams();
@@ -37,6 +38,7 @@ const Ads = () => {
         queryData();
     }, []);
 
+
     return (
         <> {adData.length > 0 && adData[0].id != 'default' ?
             <Table aria-label="Example static collection table">
@@ -54,7 +56,7 @@ const Ads = () => {
                             <TableCell><SwitchComponent checked={item.status == "ACTIVE"} onChange={() => { }} /></TableCell>
                             <TableCell>{item.id}</TableCell>
                             <TableCell>{item.name}</TableCell>
-                            <TableCell>{item.preview_shareable_link}</TableCell>
+                            <TableCell><Link href={item.preview_shareable_link} className="underline text-blue-600">{item.preview_shareable_link}</Link></TableCell>
                             <TableCell>{item.adset_id}</TableCell>
                             <TableCell>
                                 <div className="flex">
@@ -65,7 +67,7 @@ const Ads = () => {
                         </TableRow>
                     ))}
                 </TableBody>
-            </Table> : <div className="flex justify-center">
+            </Table> : adData.length === 0 ? <p>No Ad Found</p> : <div className="flex justify-center">
                 <div role="status">
                     <svg aria-hidden="true" className="mr-2 inline w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
