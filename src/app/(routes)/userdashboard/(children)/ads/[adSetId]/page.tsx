47515bcb4abbf63fc00d86b0aa7ecc3314b3bcf1
ronakpustack <ronak@pustack.com>
2024-02-20 15:05:30 +0530

    Resolced ads query

--- src/app/(routes)/userdashboard/(children)/ads/[adSetId]/page.tsx
+++ src/app/(routes)/userdashboard/(children)/ads/[adSetId]/page.tsx
@@ -30,12 +30,8 @@ const Ads = () => {
                 return;
             }
 
-            const finalList = data.filter((item: any) => item.adset_id !== adSetId);
-
-            console.log(data)
-            console.log(finalList)
-
-            updateAdData(finalList)
+            const adsForAdset = data.filter((item: any) => item.adset_id === adSetId);
+            updateAdData(adsForAdset)
         }
 
         queryData();
@@ -49,6 +45,7 @@ const Ads = () => {
                     <TableColumn>ID</TableColumn>
                     <TableColumn>Name</TableColumn>
                     <TableColumn>Shareable Link</TableColumn>
+                    <TableColumn>Adset ID</TableColumn>
                     <TableColumn>Action</TableColumn>
                 </TableHeader>
                 <TableBody>
@@ -58,6 +55,7 @@ const Ads = () => {
                             <TableCell>{item.id}</TableCell>
                             <TableCell>{item.name}</TableCell>
                             <TableCell>{item.preview_shareable_link}</TableCell>
+                            <TableCell>{item.adset_id}</TableCell>
                             <TableCell>
                                 <div className="flex">
                                     <div className="p-1 mr-2 bg-slate-300 rounded-md"><MdEdit /></div>
@@ -67,7 +65,7 @@ const Ads = () => {
                         </TableRow>
                     ))}
                 </TableBody>
-            </Table> : <div className="flex content-center">
+            </Table> : <div className="flex justify-center">
                 <div role="status">
                     <svg aria-hidden="true" className="mr-2 inline w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
--- src/app/(routes)/userdashboard/(children)/adsets/[campaignId]/page.tsx
+++ src/app/(routes)/userdashboard/(children)/adsets/[campaignId]/page.tsx
@@ -24,9 +24,8 @@ const Adsets = () => {
                 return;
             }
 
-            const finalList = data.filter((item: any) => item.campaignId !== campaignId);
-
-            updateAdsetData(finalList)
+            const adsetForCampaign = data.filter((item: any) => item.campaignId === campaignId);
+            updateAdsetData(adsetForCampaign)
         }
 
         queryData();
@@ -59,7 +58,7 @@ const Adsets = () => {
                         </TableRow>
                     ))}
                 </TableBody>
-            </Table> : <div className="flex content-center">
+            </Table> : <div className="flex justify-center">
                 <div role="status">
                     <svg aria-hidden="true" className="mr-2 inline w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
