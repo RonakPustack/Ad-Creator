1202e00237ada20f717ac32ae56c094d31ce8633
ronakpustack <ronak@pustack.com>
2024-02-23 11:30:42 +0530

    App config changes

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,20 +3,46 @@ import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
-// Production
+// Production PuStack App
 // clientId: "645064660474863",
 // clientSecret: "cdcb46d7a5014b2e9d6b9936c4156c0d",
 
+// Dev Arti AI App
+// clientId: "2066246223759250",
+// clientSecret: "d7213e6af43684e9c5fc23513571520c",
 
 // Dev
 // clientId: "713068484341612",
 // clientSecret: "e7afa5fce3a6e6c5f10fc530eeb0346f",
+interface FacebookAppConfig {
+    clientId: string;
+    clientSecret: string;
+}
 
+const getFacebookApp = (key: string): FacebookAppConfig | undefined => {
+    const config: Record<string, FacebookAppConfig> = {
+        "production_pustack": {
+            clientId: "645064660474863",
+            clientSecret: "cdcb46d7a5014b2e9d6b9936c4156c0d",
+        },
+        "production_artiai": {
+            clientId: "2066246223759250",
+            clientSecret: "d7213e6af43684e9c5fc23513571520c",
+        },
+        "dev_artiai_auth": {
+            clientId: "713068484341612",
+            clientSecret: "e7afa5fce3a6e6c5f10fc530eeb0346f",
+        }
+    }
+
+    return config[key];
+}
 export const options: NextAuthOptions = {
     providers: [
         FacebookProvider({
-            clientId: "645064660474863",
-            clientSecret: "cdcb46d7a5014b2e9d6b9936c4156c0d",
+            clientId: getFacebookApp("production_artiai")?.clientId!,
+            clientSecret: getFacebookApp("production_artiai")?.clientSecret!,
+            authorization: "https://www.facebook.com/v11.0/dialog/oauth?scope=ads_management",
             // idToken: true,
             profile(profile, token) {
                 return {
