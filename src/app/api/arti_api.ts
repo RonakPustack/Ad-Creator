067904243ede39492ea1f75406246978ad79f25c
ronakpustack <ronak@pustack.com>
2024-02-26 18:38:23 +0530

    set is local to false

--- src/app/api/arti_api.ts
+++ src/app/api/arti_api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { adCreative, countryAPI, social } from "./services";
 
-const isLocal = true
+const isLocal = false
 
 const localApiConfig = {
     baseUrl: "localhost:8081",
